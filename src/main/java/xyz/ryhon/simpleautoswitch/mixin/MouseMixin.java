96ff936b1a1945a23c69879e1a2a863b4e2f8ad0
Ryhon <ryhon@ryhn.link>
2024-10-24 20:07:35 +0200

    Update to 1.21.2-3

---
+++ src/main/java/xyz/ryhon/simpleautoswitch/mixin/MouseMixin.java
@@ -0,0 +1,24 @@
+package xyz.ryhon.simpleautoswitch.mixin;
+
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.client.Mouse;
+import xyz.ryhon.simpleautoswitch.SimpleAutoswitch;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mouse.class)
+public abstract class MouseMixin {
+	@Inject(at = @At("TAIL"), method = "onMouseScroll")
+	private void scrollInHotbar(long window, double horizontal, double vertical, CallbackInfo info) {
+		PlayerInventory lv = ((Mouse)(Object)this).client.player.getInventory();
+
+		if(lv.selectedSlot != SimpleAutoswitch.previousSlot)
+		{
+			SimpleAutoswitch.tempDisabled = true;
+			SimpleAutoswitch.previousSlot = lv.selectedSlot;
+		}
+	}
+}
